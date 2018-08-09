75bbcd35e4b8d771d9e49a455d4ef31f71017b82
tim.huang <hc092783@gmail.com>
2018-08-09 20:05:45 +0800

    react build success

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,3 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="..\\templates",
+    static_folder="..\\static",
+    static_url_path="/backend/static")
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,11 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=10000)
